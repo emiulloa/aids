a9f0811763e54ef994b713fb06092bedf56d8f75
emiulloa <145053598+emiulloa@users.noreply.github.com>
2023-09-14 12:44:55 -0600

    tipcalculoator.py

---
+++ tipcalculoator.py
@@ -0,0 +1,9 @@
+def name():
+  print("how mutch did u pay?")
+  price = input()
+  print("do you want to tip?")
+  variable = input()
+  result = int(price)*.20
+  print("heres how much you have to pay", result)
+
+name()
