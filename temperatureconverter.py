8b5207035ea387bf7c853164c62b999c99f799d4
emiulloa <145053598+emiulloa@users.noreply.github.com>
2023-09-14 12:43:22 -0600

    Create temperatureconverter.py

---
+++ temperatureconverter.py
@@ -0,0 +1,9 @@
+def name():
+  print("hello, hows the weather?")
+  variable = input()
+  print("how much degrees celsius is it today?")
+  celsius = input()
+  result = int(celsius)+32
+  print("this is the weather", result)
+
+name()
