b2d6506fe6519fc9312b1bbeddc0f21dc8dd3aa4
emiulloa <145053598+emiulloa@users.noreply.github.com>
2023-09-14 12:50:50 -0600

    Create examp1.py

---
+++ examp1.py
@@ -0,0 +1,17 @@
+def name():
+  
+ print("total de sillas adultas que va a rentar ")
+ adultos= input()
+ print("total de sillas de niños que va a rentar")
+ niños= input()
+ result1 = int(adultos)*1+int(niños)*z
+ print("esta es la cantidad de sillas",result1)
+ sillas= input()
+ result2 = int(sillas)/10
+ print("esta es la cantidad de mesas",result2)
+ mesas= input()
+ result3 = int(adultos)*25+int(niños)*20+int(mesas)*170
+ print("este es el costo",result3)
+
+name()
+
