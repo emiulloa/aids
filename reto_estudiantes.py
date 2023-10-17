e8cb0f58ac683e8c38214dd6d379c02c4f9b79b0
emiulloa <145053598+emiulloa@users.noreply.github.com>
2023-10-17 12:33:04 -0600

    Create reto_estudiantes.py

---
+++ reto_estudiantes.py
@@ -0,0 +1,51 @@
+https://replit.com/join/zylpcmixiy-emilianogarci39
+
+
+
+Averageg = input("enter you average grade: ")
+classp = input("have you participated in class,yes or no: ")
+project = input("enter your project score: ")
+
+if classp == "yes":
+  print("you are in good academic standing")
+
+elif classp == "no":
+  print("you have not participated in class you r not in good academic standing")
+
+else:
+  print("invalid input " )
+
+
+
+
+
+x = int(Averageg )  
+
+if x > 75:
+  print("you are doing good in your class")
+
+if x < 75:
+  print("you r not doing good in class")
+
+if x < 60 and classp == "no":
+  print("the student needs to improve their performance")
+  
+
+y = int(project )
+
+if y > 90:
+  print("the student recieves a disctintion")
+
+if y < 90:
+  print("no distinction for u :(")
+
+
+
+
+
+
+
+
+
+
+ 
